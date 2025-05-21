a0c76cfce225eff5a312a42226c60b767b6e0ac7
Almoatasem <moatasem.elemam20@gmail.com>
2025-05-21 10:52:06 -0700

    add multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,8 @@
-const arg = process.argv[2];
-const num = Number(arg);
-if(!isNaN(num)){for(let i=0; i<num; i++){console.log("C is fun");}}
-else {console.log("Missing number of occurences");}
+const x = Number(process.argv[2]);
+
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) console.log('C is fun');
+}
