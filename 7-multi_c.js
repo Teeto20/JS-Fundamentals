75f78ee4bc1129d3a701d5dc21b1e70725213e1f
Almoatasem <moatasem.elemam20@gmail.com>
2025-05-21 09:06:33 -0700

    multi funny c

---
+++ 7-multi_c.js
@@ -0,0 +1,7 @@
+const c = "C is fun";
+const arg = process.argv[2];
+const num = Number(arg);
+if(isNaN(num)){console.log("Missing number of occurences");}
+else 
+{ for(let i = 0; i < num; i++)
+console.log(c);}
