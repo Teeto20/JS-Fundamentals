a6e7179e9f59cbf5937d52d3770759857e8f3ba5
Almoatasem <moatasem.elemam20@gmail.com>
2025-05-22 12:52:33 -0700

    factorization

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,6 +1,5 @@
 const arg = Number(process.argv[2]); let fact = 1;
-function factorial(a){if(isNaN(a)){return 1;}
-else {for (let i=a; i>0; i--){
-fact *= i;}}return fact;}
+function factorial(a){if(isNaN(a)|| a === 0){return 1;}
+else {fact = a*factorial(a-1)}return fact;}
 console.log(factorial(arg));
 
